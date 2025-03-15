d011d15bc52ad98f3603cb0f9b531ab3a1abc6fe
Dan Mastrian <danmas@gmail.com>
2025-03-14 18:57:19 -0700

    Rainbow

--- byt-descendants-portal.ino
+++ byt-descendants-portal.ino
@@ -48,7 +48,7 @@ void setup()
 }
 
 double fps = 0.0;
-int currentPixel = 0;
+//int currentPixel = 0;
 
 void loop()
 {
@@ -59,9 +59,10 @@ void loop()
   strip.setBrightness(sysConfig.brightness);
   strip.clear();
 
-  strip.setPixelColor(currentPixel++, 0, 0, 0, 255);
-  if (currentPixel >= LED_COUNT)
-    currentPixel = 0;
+  //strip.setPixelColor(currentPixel++, 0, 0, 0, 255);
+  //if (currentPixel >= LED_COUNT)
+  //  currentPixel = 0;
+  rainbow(0);
   
   strip.show();
 
@@ -75,21 +76,14 @@ void loop()
 // Rainbow cycle along whole strip. Pass delay time (in ms) between frames.
 void rainbow(int wait)
 {
-  // Hue of first pixel runs 5 complete loops through the color wheel.
-  // Color wheel has a range of 65536 but it's OK if we roll over, so
-  // just count from 0 to 5*65536. Adding 256 to firstPixelHue each time
-  // means we'll make 5*65536/256 = 1280 passes through this loop:
-  for (long firstPixelHue = 0; firstPixelHue < 5 * 65536; firstPixelHue += 256)
-  {
-    // strip.rainbow() can take a single argument (first pixel hue) or
-    // optionally a few extras: number of rainbow repetitions (default 1),
-    // saturation and value (brightness) (both 0-255, similar to the
-    // ColorHSV() function, default 255), and a true/false flag for whether
-    // to apply gamma correction to provide 'truer' colors (default true).
-    strip.rainbow(firstPixelHue);
-    // Above line is equivalent to:
-    // strip.rainbow(firstPixelHue, 1, 255, 255, true);
-    strip.show(); // Update strip with new contents
-    delay(wait);  // Pause for a moment
-  }
+  long firstPixelHue = millis() % 65536;
+
+  // strip.rainbow() can take a single argument (first pixel hue) or
+  // optionally a few extras: number of rainbow repetitions (default 1),
+  // saturation and value (brightness) (both 0-255, similar to the
+  // ColorHSV() function, default 255), and a true/false flag for whether
+  // to apply gamma correction to provide 'truer' colors (default true).
+  strip.rainbow(firstPixelHue);
+  // Above line is equivalent to:
+  // strip.rainbow(firstPixelHue, 1, 255, 255, true);
 }
